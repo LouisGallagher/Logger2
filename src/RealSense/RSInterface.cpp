812f24a6c5061580911f0e296164d2740a5c7d0e
Louis <louis.gallagher.2013@mumail.ie>
2016-09-26 14:47:52 +0100

    Enable IR emitter.

--- src/RealSense/RSInterface.cpp
+++ src/RealSense/RSInterface.cpp
@@ -29,7 +29,10 @@
  	device->set_frame_callback(rs::stream::depth, dcb);
  	device->set_frame_callback(rs::stream::color, ccb);
 
- 	device->set_option(rs::option::r200_lr_auto_exposure_enabled, true); // //rs::option::r200_lr_exposure, 330)
+ 	//device->set_option(rs::option::r200_lr_exposure, 130); //r200_lr_auto_exposure_enabled, true //rs::option::r200_lr_exposure, 330
+	device->set_option(rs::option::r200_emitter_enabled, true);
+	//device->set_option(rs::option::r200_depth_units, 100);
+ 
  	//start streams
  	device->start(); 
 
